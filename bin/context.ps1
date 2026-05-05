cdfe65bd821f8cb7ee19be88fee6ae7def3363ad
Matthew Coulter <53892067+mattcoulter7@users.noreply.github.com>
2026-05-05 21:12:16 +1000

    Create context.ps1

---
+++ bin/context.ps1
@@ -0,0 +1,14 @@
+Get-ChildItem -Recurse -File -Include *.py,*.md |
+  Where-Object { $_.FullName -notmatch '\\.venv\\' } |
+  Sort-Object FullName |
+  ForEach-Object {
+    $relativePath = Resolve-Path -Relative $_.FullName
+    $ext = $_.Extension.TrimStart(".")
+    @"
+$relativePath
+````$ext
+$(Get-Content $_.FullName -Raw)
+`````
+
+"@
+} | Set-Clipboard

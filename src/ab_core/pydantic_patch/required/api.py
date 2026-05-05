f5648ed5213c05e428f6c5439ec52918dfb9ca2d
Matthew Coulter <53892067+mattcoulter7@users.noreply.github.com>
2026-05-05 21:55:12 +1000

    Remove generic_preserver; use Operation.source_model
    Replace generic-preserver-based generic classes with a common Operation base that provides the source model. The five APIs (omit, partial, patch, pick, required) now inherit from ab_core.pydantic_patch.core.operation.Operation, removing generic_preserver and canonical_key usage, related casts and long explanatory comments. Each API now calls its create_*_model using cls.source_model and cleans up unused imports.

--- src/ab_core/pydantic_patch/required/api.py
+++ src/ab_core/pydantic_patch/required/api.py
@@ -1,18 +1,15 @@
 """Public Required API."""
 
 from collections.abc import Collection
-from typing import Generic, TypeVar, cast
 
-from generic_preserver.wrapper import generic_preserver
-from generic_preserver.utils import canonical_key
 from pydantic import BaseModel
 
+from ab_core.pydantic_patch.core.operation import Operation
 from ab_core.pydantic_patch.required.config import RequiredConfig
 from ab_core.pydantic_patch.required.operation import create_required_model
 
 
-@generic_preserver
-class Required[T: BaseModel]():
+class Required(Operation):
     """Create a model where selected fields are required."""
 
     def __new__(
@@ -22,41 +19,8 @@ class Required[T: BaseModel]():
         child_models: dict[type[BaseModel], RequiredConfig] | None = None,
         name: str | None = None,
     ) -> type[BaseModel]:
-        # NOTE: `generic-preserver` allows us to retrieve the type instance passed to the generic class,
-        #       which is how we get the source model.
-        #
-        #       Whilst this is really convenient, `generic-preserver` intended us to access the generic
-        #       arg via self[T]. However, since we are in the `__new__` part of the object instantiation
-        #       lifecycle, the instance (self) has not yet been created, so we cannot access self[T].
-        #
-        #       Instead, we can read the `generic-preserver` state directly from the class (cls) via
-        #       `cls.__generic_map__`. Since the GenericMeta actually creates a new class for each unique
-        #       set of generic args, the `cls` in this context is already the unique class, therefore we
-        #       don't need to worry about conflicts between different generic args.
-        #
-        #       Whilst this isn't ideal, and it should be noted that this isn't how `generic-preserver` is
-        #       intended to be used, it works and allows us to have a really clean API for users of our library,
-        #       so it's a worthwhile tradeoff IMO.
-        #
-        #       It should also be noted that this relies on string serialisation of the generic arg (canonical_key(T))
-        #       to retrieve the source model, which feels a bit hacky, but important to note that this canonical_key
-        #       method comes from the same generic-preserver library, so I don't anticipate it breaking.
-        # 
-        #       Just being cautious since we've observed this happening between Python 3.11 and 3.12, where the string
-        #       serialisation of generic args changed from "~T" to "T", respectively.
-        #
-        #       However, since this is an internal implementation detail of our library, we can easily update our code
-        #       to accommodate any changes in the string serialisation of generic args in future versions of Python, so
-        #       it's not a major concern.
-        #
-
-        generic_map = cast(
-            dict[str, type[BaseModel]],
-            getattr(cls, "__generic_map__", None),
-        )
-        source_model = generic_map[canonical_key(T)]
         return create_required_model(
-            source_model,
+            cls.source_model,
             fields=fields,
             child_models=child_models,
             name=name,
